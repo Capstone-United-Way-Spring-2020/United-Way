ebdfe7407f9b818519beb05bebfc83cae299ead2
Jkritz24 <43358653+Jkritz24@users.noreply.github.com>
2020-02-05 15:33:50 -0600

    Add files via upload

---
+++ SavedChanges Class.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsAppDraft
+{
+    class SavedChanges
+    {
+        private string changeId;
+        private string userId;
+        private DateTime changeTime;
+
+        public SavedChanges()
+        {
+        }
+
+        public SavedChanges(string changeId, string userId, DateTime changeTime)
+        {
+            this.changeId = changeId;
+            this.userId = userId;
+            this.changeTime = changeTime;
+        }
+
+        public string ChangeId { get => changeId; }
+        public string UserId { get => userId; }
+        public DateTime ChangeTime { get => changeTime; }
+    }
+}
