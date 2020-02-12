2cb09ac3a043ddca0400e8ed59e790ddc185c378
Jkritz24 <43358653+Jkritz24@users.noreply.github.com>
2020-02-12 15:05:11 -0600

    Add files via upload

---
+++ AgencyModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ModelCreations.Models
+{
+    public class AgencyModel
+    {
+        public string AgencyId { get; set; }
+
+        public string AgencyName { get; set; }
+
+        public bool AgencyStatus { get; set; } = true;
+
+        public DateTime AgencyDateCreated { get; set; } = DateTime.Now;
+
+        public DateTime AgencyLastEdited { get; set; } = DateTime.Now;
+    }
+}
